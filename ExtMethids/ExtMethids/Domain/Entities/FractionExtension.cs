f637f360a4886e77d48b8e87dc8f76aa1d2f1168
Egor <arnoldtagirov1@gmail.com>
2025-11-19 15:44:18 +0100

    -m

---
+++ ExtMethids/ExtMethids/Domain/Entities/FractionExtension.cs
@@ -0,0 +1,29 @@
+﻿namespace ExtMethids.Domain.Entities
+{
+    public static  class FractionExtension
+    {
+        public static Fraction_R MinFractionInFractionArray(this Fraction_R[] fraction_array)
+        {
+            Fraction_R MinFraction = fraction_array[0];
+            foreach (var x in fraction_array)
+            {
+                if (x.Numerator < MinFraction.Numerator || x.Denominator < MinFraction.Denominator)
+                    MinFraction = x;
+            }
+            return MinFraction;
+
+        }
+        public static Fraction_R MaxFractionInFractionArray(this List<Fraction_R> fraction_array)
+        {
+            Fraction_R MinFraction = fraction_array[0];
+            foreach (var x in fraction_array)
+            {
+                if (x.Numerator > MinFraction.Numerator || x.Denominator > MinFraction.Denominator)
+                    MinFraction = x;
+            }
+            return MinFraction;
+
+        }
+
+    }
+}
